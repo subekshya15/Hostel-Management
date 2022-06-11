4f0fa1045166d3765e06bdab28da9a28143d5c8b
Subekshya <subekshya15@gmail.com>
2022-06-11 18:40:44 +0545

    Initial commit

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>Copyright &copy;Deerwalk Group</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
